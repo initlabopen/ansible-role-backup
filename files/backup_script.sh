4a824e7f1f1ecfa52c2cb886bcc5d7c2c9b5e76d
Dmitry Kochetov <d.kochetov@initlab.ru>
2017-05-19 09:24:38 +0300

    fix

--- files/backup_script.sh
+++ files/backup_script.sh
@@ -12,6 +12,24 @@ find $2 -type f -mtime +$6 -delete
 cd $2
 if [ $5 ]
 then
-nice -n 19 mysqldump -u$3 -p$4 --lock-tables=false --default-character-set=utf8 $5 | gzip > $2/$5_${date}.sql.gz
+  if [ $4 ]
+  then
+          nice -n 19 mysqldump -u$3 -p$4 --lock-tables=false --default-character-set=utf8 $5 | gzip > $2/$5_${date}.sql.gz
+  else
+          nice -n 19 mysqldump -u$3 --lock-tables=false --default-character-set=utf8 $5 | gzip > $2/$5_${date}.sql.gz
+  fi
 fi
 nice -n 19 tar cvzf $2/backup_${date}.tar.gz -X $2'/../bin/exclude.txt' $1 > /dev/null
+
+data_info_file=$(stat /tmp/backup_info |grep -e Modify -e Модифицирован | awk '{print $2}' 2>/dev/null)
+if [ `test -f /tmp/backup_info && echo "yes"` ]
+then
+        if [ "$data_info_file" != `date +%Y-%m-%d` ]
+        then
+                echo "$1 $date" > /tmp/backup_info
+        else
+                echo "$1 $date" >> /tmp/backup_info
+        fi
+else
+        echo "$1 $date" > /tmp/backup_info
+fi
